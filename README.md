9ad63f9b4337f234dc3f36d7f3a19a389c1a4933
Jimmy García <jimmyegc@gmail.com>
2025-07-16 16:22:04 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Cómo crear un portafolio profesional con React, TypeScript y Tailwind
+https://www.youtube.com/watch?v=kP0Z8sMPFjU
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Header } from "./components"
+
+function App() {
+  
+  return (    
+    <div className='bg-slate-50'>
+      <main className="container py-10">
+        <Header />      
+      </main>
+    </div>    
+  )
+}
+
+export default App
