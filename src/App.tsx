815b21738937044f9d9892a79dfd04d046e2c42b
Jimmy García <jimmyegc@gmail.com>
2025-07-18 12:44:53 -0600

    Navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import { Header } from "./components"
+import { useState } from "react"
+import { Header, Navigation } from "./components"
+import type { Project } from "./types";
 
 function App() {
+
+  const [tabActiveIndex, setActiveIndex] = useState<number>(1)
+  const [selectedProject, setSelectedProject] = useState<Project | null>(null);
   
   return (    
-    <div className='bg-slate-50'>
+    <div className='bg-slate-50 min-h-screen'>
       <main className="container py-10">
         <Header />      
+        <Navigation 
+          tabActiveIndex={tabActiveIndex}
+          setTabActiveIndex={setActiveIndex}
+        />
       </main>
     </div>    
   )
